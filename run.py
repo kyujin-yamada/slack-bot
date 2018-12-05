dc117b4060dfafb8e98760f60e5cf0bca9b0e425
kyujin-yamada <ryouya.gin@gmail.com>
2018-12-06 00:54:04 +0900

    add time rep

--- run.py
+++ run.py
@@ -4,5 +4,14 @@ def main():
     bot = Bot()
     bot.run()
 
+@respond_to('今何時？')
+def now(message):
+    strftime = datetime.now().strftime("%Y/%m/%d %H時%M分%S秒だよ～")
+    message.reply(strftime)
+
+def main():
+    bot = Bot()
+    bot.run()
+
 if __name__ == "__main__":
     main()
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,2 +1,4 @@
 DEFAULT_REPLY = "僕は Cre-one の bot だよ\nこれからいろんな機能を増やしていくよ！"
-
+PLUGINS = [
+    "slackbot.plugins"
+]
