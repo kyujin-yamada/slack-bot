6d61139baba751e46828fd5050fdf04994c7bc80
Yamada_Ryouya <ryamada@example.com>
2018-12-09 23:03:26 +0900

    bug fix

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+#coding=utf-8
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+from datetime import datetime
 
 def main():
     bot = Bot()
